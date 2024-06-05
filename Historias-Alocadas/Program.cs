18589e1c8f06f4dc333f8349e771e605677b00a4
Claudia <claudiadurantupano@gmail.com>
2024-06-05 17:39:08 -0400

    Historias Alocadas
    ejercicios que genera una frase tomando el resultado aleatorio de tres listas diferentes

---
+++ Historias-Alocadas/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+List<string> Personajes = new List<string>() { "Kimberly", "Claudia", "Emanuel","Daniel"};
+List<string> Lugares = new List<string>() {"El valle", "La cueva", "El mar", "La ciudad" };
+List<string> Evento = new List<string>() { "Viajo a", "Se durmio en", "Canto en", "Tuvo un retiro en "  };
+
+Random random = new Random();
+string palabra1 = Personajes[random.Next(Personajes.Count)];
+string palabra2 = Evento[random.Next(Evento.Count)];
+string palabra3 = Lugares[random.Next(Lugares.Count)];
+
+Console.WriteLine(palabra1 + " " + palabra2 + " " + palabra3);
